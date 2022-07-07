bb964299fff6903865e23c9e708d3b464f7c7177
snm654 <ruengermany@yandex.by>
2022-07-07 17:17:36 +0300

    fix: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n) + ''
+    let result = n[n.length - 1]
+    for (let i = n.length - 2; i >= 0; i -= 1) {
+        result = result + n[i];
+    }
+    return result
 }
